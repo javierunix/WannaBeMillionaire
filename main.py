fb924f6501581e5c0c6a69b27bdaa45d55107795
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-05-25 20:00:23 +0200

    random order in answers

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import random
 # This is my main script
+question1 = {"What is a prokaryotic cell?": ["A cell without differentiated nucleus", "A cell with differentiated nucleus", "A cell able to do photosynthesis", "A cell not able to do photosynthesis"]}
+
+my_list = [0, 1, 2, 3] # list with the number of possible answers
+
+for key, value in question1.items(): # iterate over dictionary
+    print(key) # print the question
+    random.shuffle(my_list) # make the list random
+    for i in my_list:
+        print(value[i])
+    
