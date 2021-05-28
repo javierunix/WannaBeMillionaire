7ba8554d36d6f7feaa9a034167442cb8561c4526
Francisco Javier Guijarro <fjguijarromartinez@gmail.com>
2021-05-28 14:47:08 +0200

    completed random selection

--- main.py
+++ main.py
@@ -10,6 +10,33 @@ with open('questions.csv', newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
 
+header = data[0] # save the first row in a variable
+
+data = data[1:] # delete the header row from the list
+
+data = random.sample(data, len(data)) # resample randomly the data
+
+data_level1 = []
+data_level2 = []
+data_level3 = []
+data_level4 = []
+data_level5 = []
+level_num = 999 # max number of questions of each dificulty level
+
+for i in range(len(data)):
+    if data[i][5] == '1' and len(data_level1) < level_num: # if the dificult of the question == 1 append to level 1
+        data_level1.append(data[i]) 
+    elif data[i][5] == '2' and len(data_level2) < level_num: # if the dificult of the question == 2 append to level 2
+        data_level2.append(data[i])
+    if data[i][5] == '3' and len(data_level3) < level_num: # if the dificult of the question == 3 append to level 3
+        data_level3.append(data[i])
+    if data[i][5] == '4' and len(data_level4) < level_num: # if the dificult of the question == 4 append to level 4
+        data_level4.append(data[i])
+    if data[i][5] == '5' and len(data_level5) < level_num: # if the dificult of the question == 5 append to level 5
+        data_level5.append(data[i]) 
+
+newdata = data_level1 + data_level2 + data_level3 + data_level4 + data_level5 # concatenate all levels list
+newdata.insert(0, header) # include header in position 0
 
 # create the Quizz Superclass
 class Quizz:
@@ -26,7 +53,7 @@ class Quizz:
 # loop for creating 15 questions from data[1] to data[15] (data[0] is ommited because it is the header)
 
 for k in range(1,16): 
-    exec(f'question{k} = Quizz(data[k])')
+    exec(f'question{k} = Quizz(newdata[k])')
 
 tprint('Who wants to be a Millionare') # a nice text ;-)
 
--- test.py
+++ test.py
@@ -21,19 +21,20 @@ data_level2 = []
 data_level3 = []
 data_level4 = []
 data_level5 = []
-level_num = 3 # max number of questions of each dificulty level
+level_num = 999 # max number of questions of each dificulty level
 
 for i in range(len(data)):
-	if data[i][5] == '1' and len(data_level1) < level_num:
-		data_level1.append(data[i])
-	elif data[i][5] == '2' and len(data_level2) < level_num:
+	if data[i][5] == '1' and len(data_level1) < level_num: # if the dificult of the question == 1 append to level 1
+		data_level1.append(data[i]) 
+	elif data[i][5] == '2' and len(data_level2) < level_num: # if the dificult of the question == 2 append to level 2
 		data_level2.append(data[i])
-	if data[i][5] == '3' and len(data_level3) < level_num:
+	if data[i][5] == '3' and len(data_level3) < level_num: # if the dificult of the question == 3 append to level 3
 		data_level3.append(data[i])
-	if data[i][5] == '4' and len(data_level4) < level_num:
+	if data[i][5] == '4' and len(data_level4) < level_num: # if the dificult of the question == 4 append to level 4
 		data_level4.append(data[i])
-	if data[i][5] == '5' and len(data_level5) < level_num:
+	if data[i][5] == '5' and len(data_level5) < level_num: # if the dificult of the question == 5 append to level 5
 		data_level5.append(data[i])	
 
-newdata = data_level1 + data_level2 + data_level3 + data_level4 + data_level5
-print(len(newdata))
+newdata = data_level1 + data_level2 + data_level3 + data_level4 + data_level5 # concatenate all levels list
+newdata.insert(0, header) # include header in position 0
+print(newdata)
